228141fa2f82862b9fa2ea5f1345acf77555e484
SardiusJay <abidoyejay@gmail.com>
2022-04-05 08:42:42 +0100

    Todo-App

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import Todo from './components/Todo';
+import Modal from './components/Modal';
+import Backdrop from './components/Backdrop';
+
+
+function App() {
+  return ( 
+  <div>
+    <h1>My Todo</h1>
+    <Todo text='Learn React' />
+    <Todo text='Master React' />
+    <Todo text='Explore the full React course' />
+    <Modal />
+    <Backdrop /> 
+    </div>
+  ); 
+}
+
+export default App;
